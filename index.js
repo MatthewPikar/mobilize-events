8b288bf4ae09d8469d0788f89819e4173c34f97a
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-02-05 13:25:26 -0500

    minified

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Created by mattpiekarczyk on 11/4/15.
+ */
+"use strict"
+
+var _ = require('lodash')
+var options = {}
+
+for(var i= 2, len=process.argv.length; i<len; i++){
+    var argument = process.argv[i].split(':')
+    options[argument[0]] = argument[1]
+}
+
+_.extend(options, {
+    resourceName: 'events',
+    resourceFormat: {
+        required$: ['sourceId','name'],
+        only$: ['id','sourceId','name','date','duration','location','description'],
+        id: 'string$',
+        sourceId: 'string$',
+        name: 'string$',
+        date: 'string$',
+        duration: 'integer$',
+        location: 'string$',
+        description: 'string$'
+    }
+})
+
+var resourceService = require('resource-service')
+require('seneca')()
+    .use('redis-transport')
+    .use(resourceService, options)
+    .listen({type:'redis', pin:'role:events,cmd:get'})
+    .listen({type:'redis', pin:'role:events,cmd:query'})
+    .listen({type:'redis', pin:'role:events,cmd:add'})
+    .listen({type:'redis', pin:'role:events,cmd:modify'})
+    .listen({type:'redis', pin:'role:events,cmd:delete'})
